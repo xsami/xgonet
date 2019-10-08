a9d37308d2d3b87d6b4db21d78d15d306eba2fc6
xsami <samil.vargas@hotmail.com>
2019-10-08 10:15:17 -0400

    add: user useful methods for filtering and finding

--- models/user.go
+++ models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 // User is the struct that contain the information of a user
 type User struct {
 	ID        int    `json:"id" bson:"_id"`
@@ -8,4 +10,89 @@ type User struct {
 	LastName  string `json:"last_name" bson:"last_name"`
 	Email     string `json:"email" bson:"email"`
 	Active    bool   `json:"active" bson:"active"`
+}
+
+// FindUserByID return the user with the given ID
+func FindUserByID(users []User, id int) (User, error) {
+
+	resultUsers := FilterUser(users, func(u User) bool {
+		return u.ID == id
+	})
+
+	if len(resultUsers) > 0 {
+		return resultUsers[0], nil
+	}
+
+	return User{}, fmt.Errorf("User with ID=%v was not found", id)
+}
+
+// FindUserByUsername return the user with the given username
+func FindUserByUsername(users []User, username string) (User, error) {
+
+	resultUsers := FilterUser(users, func(u User) bool {
+		return u.Username == username
+	})
+
+	if len(resultUsers) > 0 {
+		return resultUsers[0], nil
+	}
+
+	return User{}, fmt.Errorf("User with username=%v was not found", username)
+}
+
+// FindUserByEmail return the user with the given email
+func FindUserByEmail(users []User, email string) (User, error) {
+
+	resultUsers := FilterUser(users, func(u User) bool {
+		return u.Email == email
+	})
+
+	if len(resultUsers) > 0 {
+		return resultUsers[0], nil
+	}
+
+	return User{}, fmt.Errorf("User with email=%v was not found", email)
+}
+
+// FindUsersByFirstName return a slice of users with the given firstName
+func FindUsersByFirstName(users []User, firstName string) []User {
+
+	return FilterUser(users, func(u User) bool {
+		return u.FirstName == firstName
+	})
+
+}
+
+// FindUsersByLastName return a slice of users with the given lastName
+func FindUsersByLastName(users []User, lastName string) []User {
+
+	return FilterUser(users, func(u User) bool {
+		return u.LastName == lastName
+	})
+
+}
+
+// FindActiveUsers return a slice of user with status active = true
+func FindActiveUsers(users []User) []User {
+
+	return FilterUser(users, func(u User) bool {
+		return u.Active == true
+	})
+
+}
+
+// FilterUser return a slice of type User.
+// Given a slice of type User, this function evaluate the condition
+// that is passed as parameter and return a new slice of slice
+// where the condition meet the evaluator
+func FilterUser(users []User, evaluator func(u User) bool) []User {
+
+	resultArray := []User{}
+	for _, user := range users {
+		if evaluator(user) {
+			resultArray = append(resultArray, user)
+		}
+	}
+
+	return resultArray
 }
