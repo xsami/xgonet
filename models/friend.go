344ce2ac8a88aabb8f09f732acb2b410f0cb2c7b
xsami <samil.vargas@hotmail.com>
2019-10-08 23:06:11 -0400

    friend relationshipt finder methods

--- models/friend.go
+++ models/friend.go
@@ -43,6 +43,43 @@ func FindUserFriends(friends []Friend, user User) []User {
 	return resultUsers
 }
 
+// ValidateFriendShip return a boolean (true) value whenever 2 users are friends
+func ValidateFriendShip(friends []Friend, userIDA int, userIDB int) bool {
+
+	resultFriends := FilterFriends(friends, func(f Friend) bool {
+		return (f.UserIDFrom == userIDA && f.UserIDTo == userIDB) ||
+			(f.UserIDFrom == userIDB && f.UserIDTo == userIDA)
+	})
+
+	if len(resultFriends) > 0 {
+		return true
+	}
+
+	return false
+}
+
+// FindTwoUserRelationShip check how can two users be releated
+//
+// counter parameter must always start passing 0 by parameter
+func FindTwoUserRelationShip(friends []Friend, userA User, userB User, counter int) ([]User, int) {
+
+	if ValidateFriendShip(friends, userA.ID, userB.ID) {
+		return []User{}, counter // TODO: hold the users where the relationship begins
+	}
+
+	userAFriends := FindUserFriends(friends, userA)
+	userBFriends := FindUserFriends(friends, userB)
+
+	// TODO: fix circular relationship. Threshold to exit the application
+	for _, uA := range userAFriends {
+		for _, uB := range userBFriends {
+			return FindTwoUserRelationShip(friends, uA, uB, counter+1)
+		}
+	}
+
+	return []User{}, 0
+}
+
 // FilterFriends return a slice of type Friend.
 // Given a slice of type Friend, this function evaluate the condition
 // that is passed as parameter and return a new slice of Friends
