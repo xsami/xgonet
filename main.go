846c2a27540df1cd3f5da4b5db1179bc2acb92e2
xsami <samil.vargas@hotmail.com>
2019-10-09 23:15:32 -0400

    command parse

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	flags "github.com/jessevdk/go-flags"
+)
+
+var opts struct {
+	Debug    bool              `short:"d" long:"debug" description:"Show debug information"`
+	Data     string            `short:"D" long:"data" description:"Data path (absolute path)"`
+	Func     string            `short:"f" long:"function" description:"Call the specified function"`
+	Param    map[string]string `short:"p" long:"param" description:"paramters which is a map from string to string"`
+	Treshold uint              `short:"t" long:"treshold" description:"treshold of max amount of iterations"`
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
-	fmt.Println("Welcome to this social network")
+
+	if _, err := flags.ParseArgs(&opts, os.Args); err != nil {
+		fmt.Fprint(os.Stderr, err.Error()+"\n")
+		os.Exit(1)
+	}
+
+	fmt.Println(opts)
+
+}
+
+// GetOptions return the option with the paremeter passed
+func GetOptions() interface{} {
+	return opts
 }
