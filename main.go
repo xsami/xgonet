2544c1d2b53cd00f2bd8b6d4c7b298083f2901cb
xsami <samil.vargas@hotmail.com>
2019-10-11 16:42:28 -0400

    filling global variables for used packages

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"time"
 
 	flags "github.com/jessevdk/go-flags"
 	loader "github.com/xsami/xgonet/loader"
+	logger "github.com/xsami/xgonet/logger"
 	model "github.com/xsami/xgonet/models"
 )
 
@@ -30,22 +31,21 @@ func main() {
 
 	// Parse the args passed on the program execution
 	if _, err := flags.ParseArgs(&opts, os.Args); err != nil {
-		fmt.Fprint(os.Stderr, err.Error()+"\n")
-		os.Exit(1)
+		log.Fatal(err.Error() + "\n")
 	}
 
 	// Parse the mock data
 	if err := loader.LoadModel(opts.Data, &data); err != nil {
-		fmt.Fprint(os.Stderr, err.Error()+"\n")
-		os.Exit(1)
+		log.Fatal(err.Error() + "\n")
 	}
-	// Inject FriendList and UserList
+
+	// Inject FriendList, UserList and global variables for model & logger package
 	model.FriendList = data.Friends
 	model.UserList = data.Users
+	model.FriendMap = model.BuildFriendMap(data.Friends)
+	logger.Debug = opts.Debug
 
-	if opts.Debug {
-		fmt.Printf("%+v\n\n%+v\n", data, opts)
-	}
+	logger.Log("main", data, opts)
 
-	// log.Fatal(execFunction()) // Execute the called function
+	log.Fatal(execFunction()) // Execute the called function
 }
