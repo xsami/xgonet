65338a8302a17c6f715a66cf0d838345a424804e
xsami <samil.vargas@hotmail.com>
2019-10-10 22:27:18 -0400

    fix: typo on flag description

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"time"
@@ -21,7 +22,7 @@ var (
 		Debug    bool              `short:"d" long:"debug" description:"Show debug information"`
 		Data     string            `short:"D" long:"data" description:"Data path (absolute path)"`
 		Func     string            `short:"f" long:"function" description:"Call the specified function"`
-		Param    map[string]string `short:"p" long:"param" description:"paramters which is a map from string to string"`
+		Param    map[string]string `short:"p" long:"param" description:"parameters which is a map from string to string"`
 		Treshold uint              `short:"t" long:"treshold" description:"treshold of max amount of iterations"`
 	}
 	data UFStruct
@@ -45,5 +46,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("%+v\n\n%+v\n", data, opts)
+	if opts.Debug {
+		// fmt.Printf("%+v\n\n%+v\n", data, opts)
+	}
+
+	log.Fatal(execFunction()) // Execute the called function
 }
