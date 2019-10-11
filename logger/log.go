f6a07073bea61b5f748c216c85fc929566238c36
xsami <samil.vargas@hotmail.com>
2019-10-11 17:00:17 -0400

    log enhacement, display more details and separator

--- logger/log.go
+++ logger/log.go
@@ -5,6 +5,9 @@ import "fmt"
 // Debug determinate to print a message
 var Debug bool
 
+// CounterLog track the amount of calls of the functionLog
+var CounterLog int
+
 // Log check for Debug and display the passed data
 func Log(functionName string, args ...interface{}) {
 
@@ -12,11 +15,15 @@ func Log(functionName string, args ...interface{}) {
 		return
 	}
 
-	fmt.Printf("Excution in the function: %v\n", functionName)
+	const separator = "========================================"
+
+	CounterLog++
+	fmt.Printf("%v\n#%v Excution in the function: %v\n", separator, CounterLog, functionName)
 	if len(args) > 0 {
 		fmt.Println("Running the following commands:")
 	}
 	for v := range args {
 		fmt.Printf("%+v\n", v)
 	}
+	fmt.Println(separator)
 }
